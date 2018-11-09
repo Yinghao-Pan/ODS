56e2f0f849e76f41f71856555acd6b285e65c710
Yinghao Pan <ypan8@uncc.edu>
2018-11-08 23:36:33 -0500

    version 0.2.0

--- data-raw/creates_example_data.R
+++ data-raw/creates_example_data.R
@@ -59,6 +59,81 @@ tempdata <- rbind(SRS, lowerODS, upperODS, NVsample)
 ## the final example data for secondary analysis in ODS design
 ods_data_secondary <- cbind(subj_ind, tempdata)
 
+
+## create the example data set for secondary analysis in case-cohort data
+
+set.seed(1)
+lambda0 <- 1
+gamma1 <- log(1.2)
+gamma2 <- 0.5
+gamma3 <- c(-0.5, 1, 0.5)
+
+beta0 <- 1
+beta1 <- 0.5
+beta2 <- c(-0.5, 2, 3, 1)
+sigma <- 1
+
+N <- 1000          # full cohort size
+Z11 <- rbinom(N, size=1, prob=0.45)
+Z12 <- rbinom(N, size=1, prob=0.2)
+Z13 <- rbinom(N, size=1, prob=0.8)
+Z14 <-  rnorm(N, 0, 1)
+Z1 <- cbind(Z11, Z12, Z13, Z14)
+Z21 <- Z11
+Z22 <- rbinom(N, size=1, prob=0.4)
+Z23 <- rbinom(N, size=1, prob=0.3)
+Z2 <- cbind(Z21, Z22, Z23)
+Z31 <- Z11
+Z32 <- Z22
+Z33 <- Z23
+Z3 <- cbind(Z31, Z32, Z33)
+X <- rnorm(N, mean=0, sd=1)
+epsilon <- rnorm(N, mean=0, sd=sigma)
+
+Y2 <- as.vector(beta0 + beta1*X + Z1%*%beta2 + epsilon)
+failure.rate <- lambda0*exp(gamma1*Y2+gamma2*X+Z2%*%gamma3)
+tildeT <- rexp(N, rate=failure.rate)
+
+C <- runif(N, min=0, max=0.42)
+
+T <- pmin(tildeT, C)
+Delta <- ifelse(tildeT<C, 1, 0)
+Data <- cbind(T, Delta, Y2, X, Z1, Z2, Z3)
+
+### select a case-cohort sample ###
+### select a SRS sub-cohort ###
+n.SRS <- 300           # size of the SRS sub-cohort
+index <- sample(1:N, n.SRS)
+SRS <- Data[index,]
+SRS <- data.frame(SRS)
+
+### add additional cases to the case cohort sample ###
+Data.remaining <- Data[-index,]
+cases.remaining <- Data.remaining[Data.remaining[,2]==1,]
+controls.remaining <- Data.remaining[Data.remaining[,2]==0,]
+
+CCH <- rbind(SRS, cases.remaining)
+CCH <- data.frame(CCH)
+
+### note: data structure for CCH (T, Delta, Y2, X, Z1, Z2, Z3) ###
+### note: data structure for NVsample (T, Delta, Y2, Z1, Z2, Z3) ###
+### NVsample is the set where we dont observe the expensive exposure ###
+
+NVsample <- controls.remaining
+NVsample[,4] <- NA
+
+### create the subject indicator ###
+### 1 = SRS
+### 2 = supplemental cases
+### 0 = non-validation sample
+subj_ind <- c(rep(1, 300), rep(2, nrow(CCH)-300), rep(0, N-nrow(CCH)))
+
+data <- rbind(CCH, NVsample)
+data <- cbind(subj_ind, data)
+
+## the final example data for secondary analysis in case-cohort design
+casecohort_data_secondary <- data
+
 ### create the example data set for analyzing the primary outcome in ODS design ###
 ### this data set is part of the data set ods_data_secondary ###
 
